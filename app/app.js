29894e56ea9d99f209fe43a3240ff5e3098f633c
nocmar <martanocon@gmail.com>
2016-03-19 17:08:17 +0100

    added one extra level

--- app/app.js
+++ app/app.js
@@ -1,44 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ExpenseTable from "./expenseTable";
-var expenses =  [
-  {
-    "id": 1,
-    "tranactionDate": "2016-01-01",
-    "transactionDetails": "5575XXXXXXXX6717 -Zabka Polska Z3573,Wroclaw, POLna kwotę 7,77 PLN z dnia 19/02/2016",
-    "transactionBankType": "TransakcjaKarta",
-    "transactionType" : "Obciażenie",
-    "account": 123314324343,
-    "amount": 200,
-    "approved": false,
-    "category" : "Spożywcze",
-    "notes" : "jedzenie na weekend"
-  },
-  {
-    "id":2,
-    "tranactionDate": "2016-01-02",
-    "transactionDetails": "Bankomat Wroclaw",
-    "transactionBankType": "Wypłata",
-    "transactionType" : "Obciażenie",
-    "account": "",
-    "amount": 300,
-    "approved": true,
-    "category" : "Samochód",
-    "notes" : "zapłata mechanika"
-  },
-  {
-    "id":3,
-    "tranactionDate": "2016-01-02",
-    "transactionDetails": "Prad",
-    "transactionBankType": "Przelw",
-    "transactionType" : "Obciażenie",
-    "account": 123314324343,
-    "amount": 3,
-    "approved": false,
-    "category" : "Opłaty",
-    "notes" : ""
-  }
-];
+import Expenses from "./expenses"
+
 ReactDOM.render(
-  <ExpenseTable expenses={expenses}/>,  document.getElementById('app')
+  <Expenses/>,  document.getElementById('app')
 );
---
+++ app/expenses.js
@@ -0,0 +1,58 @@
+import React from "react";
+import ExpenseTable from "./expenseTable";
+
+export default class Expenses extends React.Component {
+  constructor(){
+      super();
+      var expenses =  [
+        {
+          "id": 1,
+          "tranactionDate": "2016-01-01",
+          "transactionDetails": "5575XXXXXXXX6717 -Zabka Polska Z3573,Wroclaw, POLna kwotę 7,77 PLN z dnia 19/02/2016",
+          "transactionBankType": "TransakcjaKarta",
+          "transactionType" : "Obciażenie",
+          "account": 123314324343,
+          "amount": 200,
+          "approved": false,
+          "category" : "Spożywcze",
+          "notes" : "jedzenie na weekend"
+        },
+        {
+          "id":2,
+          "tranactionDate": "2016-01-02",
+          "transactionDetails": "Bankomat Wroclaw",
+          "transactionBankType": "Wypłata",
+          "transactionType" : "Obciażenie",
+          "account": "",
+          "amount": 300,
+          "approved": true,
+          "category" : "Samochód",
+          "notes" : "zapłata mechanika"
+        },
+        {
+          "id":3,
+          "tranactionDate": "2016-01-02",
+          "transactionDetails": "Prad",
+          "transactionBankType": "Przelw",
+          "transactionType" : "Obciażenie",
+          "account": 123314324343,
+          "amount": 3,
+          "approved": false,
+          "category" : "Opłaty",
+          "notes" : ""
+        }
+      ];
+
+      this.state ={
+        expenses : expenses,
+      };
+
+
+  }
+
+render(){
+  return (
+    <ExpenseTable expenses={this.state.expenses}/>
+  );
+}
+}
