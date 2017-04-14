0d2f28efb2a499daf2c2358fbaee6f92f029af77
nocmar <martanocon@gmail.com>
2017-04-14 11:27:14 +0100

    first poc of table for budget

--- client/src/components/Budgets/budgets.js
+++ client/src/components/Budgets/budgets.js
@@ -1,10 +1,203 @@
 import React from "react";
+import * as Table from 'reactabular-table';
+
+import cloneDeep from 'lodash/cloneDeep';
+import orderBy from 'lodash/orderBy';
+import { compose } from 'redux';
+import * as resolve from 'table-resolver';
+import VisibilityToggles from 'reactabular-visibility-toggles';
+import * as tree from 'treetabular';
+import * as search from 'searchtabular';
+import * as sort from 'sortabular';
 
 export default class Budgets extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const columns = this.getColumns();
+    const rows = resolve.resolve({ columns })(this.rows);
+    this.state = {
+      searchColumn: 'all',
+      query: {},
+      sortingColumns: null,
+      rows,
+      columns
+    };
+
+    this.onExpandAll = this.onExpandAll.bind(this);
+    this.onCollapseAll = this.onCollapseAll.bind(this);
+    this.onToggleColumn = this.onToggleColumn.bind(this);
+  }
+
+  schema = {
+    type: 'object',
+    properties: {
+      id: {
+        type: 'string'
+      },
+      category: {
+        type: 'string'
+      },
+      value: {
+        type: 'integer'
+      }
+    },
+    required: ['id', 'category', 'value']
+  };
+  getColumns() {
+    const sortable = sort.sort({
+      // Point the transform to your rows. React state can work for this purpose
+      // but you can use a state manager as well.
+      getSortingColumns: () => this.state.sortingColumns || {},
+
+      // The user requested sorting, adjust the sorting state accordingly.
+      // This is a good chance to pass the request through a sorter.
+      onSort: selectedColumn => {
+        const sortingColumns = sort.byColumns({
+          sortingColumns: this.state.sortingColumns,
+          selectedColumn
+        });
+
+        this.setState({ sortingColumns });
+      }
+    });
+
+    return [
+      {
+        property: 'category',
+        props: {
+          style: { width: 200 }
+        },
+        header: {
+          label: 'Category',
+          transforms: [sortable]
+        },
+        cell: {
+          formatters: [
+            tree.toggleChildren({
+              getRows: () => this.state.rows,
+              getShowingChildren: ({ rowData }) => rowData.showingChildren,
+              toggleShowingChildren: rowIndex => {
+                const rows = cloneDeep(this.state.rows);
+
+                rows[rowIndex].showingChildren = !rows[rowIndex].showingChildren;
+
+                this.setState({ rows });
+              },
+              // Inject custom class name per row here etc.
+              props: {}
+            })
+          ]
+        },
+        visible: true
+      },
+      {
+        property: 'value',
+        props: {
+          style: { width: 300 }
+        },
+        header: {
+          label: 'Value',
+          transforms: [sortable]
+        },
+        visible: true
+      }
+    ];
+  }
+  rows = [
+    {
+      id: 123,
+      category: 'Demo',
+      value: 100
+    },
+    {
+      id: 456,
+      category: 'Another',
+      value: 100,
+      parent: 123
+    },
+    {
+      id: 789,
+      category: 'Yet Another',
+      value: 100,
+      parent: 123
+    },
+    {
+      id: 532,
+      value: 100,
+      category: 'Foobar'
+    }
+  ];
+
   render() {
-    console.log("butgets");
+    const {
+      searchColumn, columns, sortingColumns, query
+    } = this.state;
+    const visibleColumns = columns.filter(column => column.visible);
+    const rows = compose(
+      tree.filter({ fieldName: 'showingChildren' }),
+      tree.wrap({
+        operations: [
+          sort.sorter({
+            columns,
+            sortingColumns,
+            sort: orderBy
+          })
+        ]
+      }),
+      tree.search({
+        operation: search.multipleColumns({ columns, query })
+      })
+    )(this.state.rows);
+
     return (
-      <h1>Budzety</h1>
+      <div>
+        <VisibilityToggles
+          columns={columns}
+          onToggleColumn={this.onToggleColumn}
+        />
+
+        <button onClick={this.onExpandAll}>Expand all</button>
+        <button onClick={this.onCollapseAll}>Collapse all</button>
+
+        <div className="search-container">
+          <span>Search</span>
+          <search.Field
+            column={searchColumn}
+            query={query}
+            columns={visibleColumns}
+            rows={rows}
+            onColumnChange={searchColumn => this.setState({ searchColumn })}
+            onChange={query => this.setState({ query })}
+          />
+        </div>
+
+        <Table.Provider
+          className="pure-table pure-table-striped"
+          columns={visibleColumns}
+        >
+          <Table.Header />
+
+          <Table.Body rows={rows} rowKey="id" />
+        </Table.Provider>
+      </div>
     );
+  }
+  onExpandAll() {
+    this.setState({
+      rows: tree.expandAll()(this.state.rows)
+    });
+  }
+  onCollapseAll() {
+    this.setState({
+      rows: tree.collapseAll()(this.state.rows)
+    });
+  }
+  onToggleColumn({ columnIndex }) {
+    const columns = cloneDeep(this.state.columns);
+
+    columns[columnIndex].visible = !columns[columnIndex].visible;
+
+    this.setState({ columns });
   }
 }
